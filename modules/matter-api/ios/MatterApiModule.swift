66360972039cc2cb839c52fd1a44a00a608ae8ac
voxior <kozjek.nejc198@gmail.com>
2024-06-04 09:07:40 +0200

    add ios commissioning support

--- modules/matter-api/ios/MatterApiModule.swift
+++ modules/matter-api/ios/MatterApiModule.swift
@@ -1,10 +1,11 @@
 import ExpoModulesCore
-import WebKit
+import _Concurrency
+import MatterSupport
 
 public class MatterApiModule: Module {
   // Each module class must implement the definition function. The definition consists of components
   // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
+    // See https://docs.expo.dev/modules/module-api for more details about @available(iOS 16.1, *)
   public func definition() -> ModuleDefinition {
     // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
     // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
@@ -17,19 +18,40 @@ public class MatterApiModule: Module {
 
 
 class MatterView: ExpoView {
-  let webView = WKWebView()
+
 
   required init(appContext: AppContext? = nil) {
     super.init(appContext: appContext)
     clipsToBounds = true
-    addSubview(webView)
+    let button = UIButton(frame: CGRect(x:100, y:100, width:100, height: 50))
+    button.backgroundColor = .green
+    button.setTitle("Launch comissioning", for: .normal)
+    button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+    addSubview(button)
 
-    let url =  URL(string:"https://docs.expo.dev/modules/")!
-    let urlRequest = URLRequest(url:url)
-    webView.load(urlRequest)
   }
-
-  override func layoutSubviews() {
-    webView.frame = bounds
-  }
+  @objc func buttonAction(sender: UIButton!) {
+      if #available(iOS 16.1, *) {
+          let homes = [MatterAddDeviceRequest.Home(displayName: "My Home")]
+          let topology = MatterAddDeviceRequest.Topology(ecosystemName: "MyEcosystemName", homes: homes)
+          let request = MatterAddDeviceRequest(topology: topology)
+          
+          do {
+              let sem = DispatchSemaphore(value: 0)
+              let op = Task {
+                  try await request.perform()
+                  sem.signal()
+              }
+              sem.wait()
+              print("Successfully set up device!")
+         } catch {
+             print("Failed to set up device with error: \(error)")
+         }      } else {
+          // Fallback on earlier versions
+      }
+    
+    }
+  // override func layoutSubviews() {
+  //  webView.frame = bounds
+  // }
 }
